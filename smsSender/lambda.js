0e97e2ec1f3ac6b0ea4227bddc1221acfa67ed9b
Juan Pujol <contact@juanpujol.com>
2018-08-09 23:59:11 -0300

    update code

--- smsSender/lambda.js
+++ smsSender/lambda.js
@@ -1,12 +1,15 @@
 let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
-
 exports.handler = function (event, context, callback) {
+
 	let receiver = event['receiver'];
 	let sender = event['sender'];
 	let message = event['message'];
 
+	let isPromotional = true;
+
 	console.log("Sending message", message, "to receiver", receiver);
+
 	sns.publish({
 		Message: message,
 		MessageAttributes: {
@@ -22,11 +25,11 @@ exports.handler = function (event, context, callback) {
 		PhoneNumber: receiver
 	}).promise()
 		.then(data => {
-			// your code goes here
+			console.log("Sent message to", receiver);
+			callback(null, data);
 		})
 		.catch(err => {
-			// error handling goes here
+			console.log("Sending failed", err);
+			callback(err);
 		});
-
-	callback(null, 'Successfully executed');
 }
